85d4e3eda75823ad7d3602308927091f08e2be5c
sanskarsarang <sanskarsarang@gmail.com>
2024-09-30 19:51:21 +0530

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,23 @@
+/*
+#include <iostream>
+
+int main() 
+{
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    std::cout << "You entered: " << num << std::endl;
+    return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<< " enter age :"  ;
+    cin >> age;
+    cout <<  "your age is : "<< age << endl;
+        return 0;
+}
